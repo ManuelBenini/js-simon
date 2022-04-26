341532737a78cea397de5355f8a59c58194bc2d5
ManuelBenini <manuelbenini1905@gmail.com>
2022-04-26 14:16:21 +0200

    First commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+/*
+**Consegna:**
+Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 5 secondi.
+Dopo 5 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+*/
